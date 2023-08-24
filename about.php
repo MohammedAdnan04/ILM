103bb6fbb40373365c706cbbeff8049b824ba48d
Mohammed Adnan <119103118+MohammedAdnan04@users.noreply.github.com>
2023-08-24 14:34:01 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,234 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>At ILM, we go beyond being just an online bookstore. Our commitment to exceptional service, handpicked selections, and a seamless shopping experience sets us apart. Join us on a literary journey that's tailored to your passion for books.</p>
+         <p>Experience book shopping like never before with ILM. Our dedication to quality selection and seamless service makes us your trusted destination for literary exploration.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <p>The convenience of browsing and ordering books on ILM is unbeatable. The website's user-friendly design and prompt delivery have made my reading journey more enjoyable than ever,</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Mark R.</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <p>ILM is my go-to source for finding hidden literary gems. The personalized recommendations have introduced me to authors and stories I would have otherwise missed. A true book lover's paradise!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Emily W.</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <p>What sets ILM apart is their genuine passion for books. The team's dedication shines through in their thoughtfully curated selection and personalized recommendations. A must-visit for book enthusiasts!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>David M.</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <p>ILM's commitment to quality is evident in their book collection. As a devoted reader, I appreciate their attention to diverse genres and exceptional customer service. Highly recommended.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Sarah L.</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <p>they offer a journey through the world of literature. Their commitment to quality assurance and prompt delivery has made me a loyal customer. Keep up the great work!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Alex S.</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>The platform's intuitive interface, along with their quick response to queries, makes for an outstanding book shopping experience. I'm hooked!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> Jessica H.</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">greate authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/author-1.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Ethan Carter</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Olivia Bennett</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-3.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>William Morgan</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Isabella Clarke</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Nathan Hughes</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-6.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Sophia Patel</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-74118-59212 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> adnanzohan19@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bangalore, india - 560043 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/adnan.zohan.68?mibextid=ZbWKwL"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/ZohanAdnan?t=B75_C-EbIt0PFzd_AO0YSQ&s=09"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://instagram.com/adnan_zohan_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/mohammed-adnan-14562821b"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Mr. Mohammed Adnan</span> </p>
+
+</section>
